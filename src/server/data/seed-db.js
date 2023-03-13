1fa68c351f26afeb8d4d8d2129624941bac28156
kamal1182 <mkamal1182@gmail.com>
2023-03-13 20:14:45 +0200

    Update seed-db.js

--- src/server/data/seed-db.js
+++ src/server/data/seed-db.js
@@ -1,21 +1,25 @@
 require('dotenv').config();
 
 const MongoClient = require('mongodb').MongoClient;
+
 const bcrypt = require('bcrypt');
 
 const users = require('./users');
+
 const contacts = require('./contacts');
+
 require('dotenv').config();
-
-
 
 function seedCollection(collectionName, initialRecords) {
 
   MongoClient.connect(process.env.DB_CONN, { useUnifiedTopology: true }, (err, cluster) => {
     
     if(err) {
+
       console.log('Database error: ' + err);
+
     } else {
+      
       const db = cluster.db('contacts-app-vm');
       
       const collection = db.collection(collectionName);
@@ -25,23 +29,30 @@ function seedCollection(collectionName, initialRecords) {
       collection.deleteMany({});  
 
       initialRecords.forEach((item) => {
+      
         if (item.password) {
+      
           item.password = bcrypt.hashSync(item.password, 10);
+      
         }
       });
 
       console.log('inserting records...');
 
       collection.insertMany(initialRecords, (err, result) => {
+        
         console.log(`${result.insertedCount} records inserted.`);
+        
         console.log('closing connection...');
+        
         cluster.close();
+        
         console.log('done.');
+
       });
     }
   });
 }
-
 
 seedCollection('users', users);
 seedCollection('contacts', contacts);
